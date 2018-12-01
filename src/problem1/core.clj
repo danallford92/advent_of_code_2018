8ad013f19a7314f61377cc388f86c6ddf2b50580
Dan Allford <danallford_23@hotmail.com>
2018-12-01 14:04:20 +0000

    refactoring

--- src/problem1/core.clj
+++ src/problem1/core.clj
@@ -7,22 +7,27 @@
        str/split-lines
        (map (fn [x] (Integer/parseInt x)))))
 
+
 (defn run1 [in]
-  (->> in
-       parse-input
-       (reduce +))
+  (time (->> in
+             parse-input
+             (reduce +)))
   )
 
 (defn run2 [in]
-  (->> in
-       parse-input
-       cycle
-       (reductions +)
-       ((fn [y] (map (fn [a b] [a b]) y (reductions conj #{} y))))
-       (filter (fn [[n s]] (contains? s n)))
-       (first)
-       (first)
-  )
-)
+  (def cum-sum #(reductions + %))
+  (defn find-first-repeat [xs]
+    (->> xs
+         (#(map (fn [a b] [a b]) (reductions conj #{} %) %))
+         (filter #(contains? (first %) (second %)))
+         (first)
+         (second)))
+
+  (time (->> in
+             parse-input
+             cycle
+             cum-sum
+             find-first-repeat
+             )))
 
 
--- src/runner/core.clj
+++ src/runner/core.clj
@@ -1,14 +1,28 @@
 (ns runner.core
-  (:require [clojure.string :as str] [clj-http.client :as client] :require [problem1.core :as problem1]))
+  (
+    :require [clj-http.client :as client]
+             [problem1.core :as problem1]
+             [clojure.java.io :as io]))
+
+
+(defn get-problem-input [[problem-number session-id]]
+  (def cache-file (format "problem-input/%s.txt" problem-number))
+
+  (if (not (.exists (io/file cache-file)))
+    (spit cache-file
+          (:body (client/get (format "https://adventofcode.com/2018/day/%s/input" problem-number)
+                       {:headers {:Cookie (format "session=%s" session-id)}})))
+    )
+
+  (slurp cache-file)
+  )
 
 
 (defn -main
   "Takes session id as first argument to retrieve the problem input"
   [& args]
   (->>
-    (client/get "https://adventofcode.com/2018/day/1/input"
-                {:headers {:Cookie (format "session=%s" (first args))}})
-    :body
+    (get-problem-input args)
     problem1/run2
     println
     )
