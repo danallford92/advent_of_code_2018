4e377dc8d67b05836e9564f5f04022200d699bf0
Dan Allford <danallford_23@hotmail.com>
2018-12-03 10:09:32 +0000

    add todo for broken problem 1

--- src/problem1/core.clj
+++ src/problem1/core.clj
@@ -4,19 +4,24 @@
   (map #(Integer/parseInt %) in)
   )
 
-
 (defn run1 [in]
   (time (->> in
              parse-input
              (reduce +)))
   )
 
+(map f [1 2 3] [4 5 6]) -> [(f 1 4) (f 2 5) (f 3 6)]
+
+(reductions conj #{} [1 2 3 2]) -> [#{} #{1} #{1 2} #{1 2 3}] [1 2 3]
+
+[#{} 1] [#{1} 2] [#{1 2} 3] [#{1 2 3} 2]
+
 (defn find-duplicates [xs]
   (->> xs
        (#(map (fn [a b] [a b]) (reductions conj #{} %) %))
        (filter #(contains? (first %) (second %)))
        (map second)
-       (into #{})
+       (into #{})  ;TODO: should not be set (infinite seq)
        ))
 
 (defn run2 [in]
